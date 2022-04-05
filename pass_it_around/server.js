fbdfc18a9a722310e56590f81146adc4392e9d12
contactharrisc2 <contactharrisc2@gmail.com>
2022-04-04 23:16:56 -0500

    updates pass_it_around to little bugs in the code

--- pass_it_around/server.js
+++ pass_it_around/server.js
@@ -5,8 +5,11 @@ const expressServer = require("express");
 const app = expressServer();
 const PORT = process.env.PORT || 3000;
 const TYPE = "soda"
+const CAP = 200;
 let font_size = "font-size: 25px", display = "display: block", margin_top = "margin-top: 10px", margin_left = "margin-left: 175px", width = "width: fit-content", color = "color: blue";
-let num_bottles = 99;
+let num_bugs = 99;
+let largestNum = num_bugs;
+// let smallestNum = largestNum-1;
 
 //////////////////////
 // Declare Middleware
@@ -20,13 +23,73 @@ let num_bottles = 99;
 //localhost:PORT#/
 app.get("/", (req, res) =>{
     console.log("root")
-    num_bottles = 99;
+    num_bugs = 99;
     res.send(`
-    <h1> <span syle="${color}">${num_bottles}</span> Bottles of ${TYPE} on the wall.
-        <span style="color: white">${num_bottles--}</span>
-        <a href="/${num_bottles}" style="${font_size}; ${display}; ${margin_top}; ${margin_left}; ${width}">Take one down. Pass it around.</a>
-    </h1>`);
+        <h1> <span style="${color}">${num_bugs}</span> little bugs in the code.</h1>
+        <h1> <span style="${color}">${num_bugs}</span> little bugs.</h1>
+        <h1>Take one down.</h1>        
+        <p style="color: blue; position: absolute; top: 0px; right: 0px">${num_bugs--}</p>
+        <h1><a href="/${num_bugs}" style="${display};${width}">Pass it around.</a></h1>`
+    );
+});
 
+
+
+//localhost:PORT#/: (User enters number | handles request to ensure a number)
+app.get('/:bugsRemaining', (req, res)=>{
+    num_bugs = parseInt(req.params.bugsRemaining); 
+    if(!num_bugs && num_bugs !== 0){
+        res.send(`<h1>Enter a valid number</h1>`)
+    }   
+    if(num_bugs === 0){
+        res.send(`
+            <h1 style="margin-left: 65vh">Congrats! <span style="color: red">${num_bugs}</span> little bugs in the code!?! Turn in your assignment!
+                <a href="/" style="${font_size}; ${display}; ${margin_top}; ${margin_left}; ${width}; margin-left: 28vh">Play Again?</a>
+                <iframe src="https://giphy.com/embed/SfYTJuxdAbsVW" width="480" height="382" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p></p>
+            </h1>`
+        );
+    }else{
+        const percent = 0.02, num_random = +Math.random().toFixed(2);
+        console.log("Random Number: " + num_random)
+        const prev_num = num_bugs;
+        let more_bugs = false;
+        if(largestNum < CAP){
+            largestNum++;
+        }
+        if(num_random < percent){
+            // num_bugs = Math.floor(Math.random()*(largestNum - smallestNum + 1)) + smallestNum;
+            more_bugs = true;
+            num_bugs = largestNum;
+        }else{
+            largestNum--;
+        }
+        if(num_bugs < 10){
+        color = "color: red";
+        }
+
+        if(more_bugs){
+            res.send(`        
+            <h1> <span style="${color}">${prev_num}</span> little bugs in the code.</h1>
+            <h1> <span style="${color}">${prev_num}</span> little bugs</h1>
+            <h1>Take one down</h1>
+            <p style="color: blue; position: absolute; top: 0px; right: 0px">${num_bugs--}</p>
+            <h1>
+                <a href="/${num_bugs}" style="${display}; ${width}">Pass it around.</a>
+            </h1>
+            <h1><span style="color: red">${num_bugs}</span> little bugs in the code.</h1>`
+            );
+        }
+        res.send(`        
+            <h1> <span style="${color}">${num_bugs}</span> little bugs in the code.</h1>
+            <h1> <span style="${color}">${num_bugs}</span> little bugs</h1>
+            <h1>Take one down</h1>
+            <p style="color: blue; position: absolute; top: 0px; right: 0px">${num_bugs--}</p>
+            <h1>
+                <a href="/${num_bugs}" style="${display}; ${width}">Pass it around.</a>
+            </h1>`
+        );
+    }
+   
 });
 
 //localhost:PORT#/98
@@ -35,34 +98,6 @@ app.get(`/98`, (req, res) =>{
         <h1> <span style="${color}: blue">98</span> bottles of ${TYPE} on the wall.</h1> 
         <h4 style="color: purple">Enter number of bottles in place of <span style="color: blue">98</span> in the url above!</h4>`
     );
-});
-
-//localhost:PORT#/: (User enters number | handles request to ensure a number)
-app.get('/:bottlesRemaining', (req, res)=>{
-    num_bottles = parseInt(req.params.bottlesRemaining); 
-    if(!num_bottles && num_bottles !== 0){
-        res.send(`<h1>Enter a valid number</h1>`)
-    }   
-    if(num_bottles === 0){
-        res.send(`
-            <h1 style="margin-left: 65vh">You chugged it all! <span style="color: red">${num_bottles}</span> Bottles of ${TYPE} on the wall!
-                <a href="/" style="${font_size}; ${display}; ${margin_top}; ${margin_left}; ${width}; margin-left: 28vh">Play Again?</a>
-                <iframe style="margin-left: 10vh" src="https://giphy.com/embed/acIy5aKe4nryg" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p>
-            </h1>`
-        );
-    }else{
-        if(num_bottles < 10){
-        color = "color: red";
-        }
-
-        res.send(`        
-            <h1> <span style="${color}">${num_bottles}</span> Bottles of ${TYPE} on the wall.
-                <span style="color: white">${num_bottles--}</span>
-                <a href="/${num_bottles}}" style="${font_size}; ${display}; ${margin_top}; ${margin_left}; ${width}">Take one down. Pass it around.</a>
-            </h1>`
-        );
-    }
-   
 });
 
 ///////////////////////////

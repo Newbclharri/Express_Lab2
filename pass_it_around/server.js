c50c6b097333d8c055e17a929a947e90a4ac1639
contactharrisc2 <contactharrisc2@gmail.com>
2022-04-05 00:41:11 -0500

    adds random gif if num_bugs === 0

--- pass_it_around/server.js
+++ pass_it_around/server.js
@@ -9,7 +9,18 @@ const CAP = 200;
 let font_size = "font-size: 25px", display = "display: block", margin_top = "margin-top: 10px", margin_left = "margin-left: 175px", width = "width: fit-content", color = "color: blue";
 let num_bugs = 99;
 let largestNum = num_bugs;
-// let smallestNum = largestNum-1;
+const rageGifs = [
+    /*0*/ `<iframe src="https://giphy.com/embed/YGfabYi6K568aL80Eh" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`,
+    /*1*/ `<iframe src="https://giphy.com/embed/SfYTJuxdAbsVW" width="480" height="382" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`,
+    /*2*/ `<iframe src="https://giphy.com/embed/qFi3fACSMnP4Q" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`,
+    /*3*/ `<iframe src="https://giphy.com/embed/hqU9Xllx4O9uq0TjAn" width="480" height="382" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`,
+    /*4*/ `<iframe src="https://giphy.com/embed/v4LP2DsVKBHSE" width="480" height="269" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`,
+    /*5*/ `<iframe src="https://giphy.com/embed/4DbLWs63sRPwY" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`,
+    /*6*/`<iframe src="https://giphy.com/embed/NVoFUKNbJfQoE" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`,
+    /*7*/ `<iframe src="https://giphy.com/embed/H9RL1IKvsBi6c" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`,
+    /*8*/ `<iframe src="https://giphy.com/embed/EPsdF1SvWrzHy" width="480" height="320" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`,
+    /*9*/ `<iframe src="https://giphy.com/embed/12i3TW7x8vp7sQ" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`
+]
 
 //////////////////////
 // Declare Middleware
@@ -26,7 +37,7 @@ app.get("/", (req, res) =>{
     num_bugs = 99;
     res.send(`
         <h1> <span style="${color}">${num_bugs}</span> little bugs in the code.</h1>
-        <h1> <span style="${color}">${num_bugs}</span> little bugs.</h1>
+        <h1> <span style="${color}">${num_bugs}</span> little bugs</h1>
         <h1>Take one down.</h1>        
         <p style="color: blue; position: absolute; top: 0px; right: 0px">${num_bugs--}</p>
         <h1><a href="/${num_bugs}" style="${display};${width}">Pass it around.</a></h1>`
@@ -42,10 +53,11 @@ app.get('/:bugsRemaining', (req, res)=>{
         res.send(`<h1>Enter a valid number</h1>`)
     }   
     if(num_bugs === 0){
+        const gif = Math.floor(Math.random() * rageGifs.length)
         res.send(`
             <h1 style="margin-left: 65vh">Congrats! <span style="color: red">${num_bugs}</span> little bugs in the code!?! Turn in your assignment!
-                <a href="/" style="${font_size}; ${display}; ${margin_top}; ${margin_left}; ${width}; margin-left: 28vh">Play Again?</a>
-                <iframe src="https://giphy.com/embed/SfYTJuxdAbsVW" width="480" height="382" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p></p>
+                <a href="/" style="${font_size}; ${display}; ${margin_top}; ${margin_left}">Play Again?</a>
+                ${rageGifs[gif]}
             </h1>`
         );
     }else{
@@ -57,7 +69,6 @@ app.get('/:bugsRemaining', (req, res)=>{
             largestNum++;
         }
         if(num_random < percent){
-            // num_bugs = Math.floor(Math.random()*(largestNum - smallestNum + 1)) + smallestNum;
             more_bugs = true;
             num_bugs = largestNum;
         }else{
@@ -71,7 +82,7 @@ app.get('/:bugsRemaining', (req, res)=>{
             res.send(`        
             <h1> <span style="${color}">${prev_num}</span> little bugs in the code.</h1>
             <h1> <span style="${color}">${prev_num}</span> little bugs</h1>
-            <h1>Take one down</h1>
+            <h1>Take one down.</h1>
             <p style="color: blue; position: absolute; top: 0px; right: 0px">${num_bugs--}</p>
             <h1>
                 <a href="/${num_bugs}" style="${display}; ${width}">Pass it around.</a>
@@ -82,7 +93,7 @@ app.get('/:bugsRemaining', (req, res)=>{
         res.send(`        
             <h1> <span style="${color}">${num_bugs}</span> little bugs in the code.</h1>
             <h1> <span style="${color}">${num_bugs}</span> little bugs</h1>
-            <h1>Take one down</h1>
+            <h1>Take one down.</h1>
             <p style="color: blue; position: absolute; top: 0px; right: 0px">${num_bugs--}</p>
             <h1>
                 <a href="/${num_bugs}" style="${display}; ${width}">Pass it around.</a>

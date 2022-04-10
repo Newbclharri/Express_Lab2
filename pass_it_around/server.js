ec92e3f33cfa776a19fb1ef3df6469dc7cf8614d
contactharrisc2 <contactharrisc2@gmail.com>
2022-04-10 13:04:38 -0500

    Unsure as to why server throws cannot set headers after they are sent to client error

--- pass_it_around/server.js
+++ pass_it_around/server.js
@@ -9,6 +9,8 @@ const CAP = 200;
 let font_size = "font-size: 25px", display = "display: block", margin_top = "margin-top: 10px", margin_left = "margin-left: 175px", width = "width: fit-content", color = "color: blue";
 let num_bugs = 99;
 let largestNum = num_bugs;
+
+//to render randomly if num_bugs === 0
 const rageGifs = [
     /*0*/ `<iframe src="https://giphy.com/embed/YGfabYi6K568aL80Eh" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`,
     /*1*/ `<iframe src="https://giphy.com/embed/SfYTJuxdAbsVW" width="480" height="382" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`,
@@ -35,16 +37,15 @@ const rageGifs = [
 app.get("/", (req, res) =>{
     console.log("root")
     num_bugs = 99;
-    res.send(`
-        <h1> <span style="${color}">${num_bugs}</span> little bugs in the code.</h1>
-        <h1> <span style="${color}">${num_bugs}</span> little bugs</h1>
-        <h1>Take one down.</h1>        
-        <p style="color: blue; position: absolute; top: 0px; right: 0px">${num_bugs--}</p>
-        <h1><a href="/${num_bugs}" style="${display};${width}">Pass it around.</a></h1>`
-    );
+    res.send(         
+                `    
+                <h1> <span style="${color}">${num_bugs}</span> little bugs in the code.</h1>
+                <h1> <span style="${color}">${num_bugs}</span> little bugs</h1>
+                <h1>Take one down.</h1>
+                <p style="font-size: 0px; color: blue; position: absolute; top: 0px; right: 0px">${num_bugs--}</p>
+                <h1><a href="/${num_bugs}" style="${display};${width}">Pass it around.</a></h1>`
+    );    
 });
-
-
 
 //localhost:PORT#/: (User enters number | handles request to ensure a number)
 app.get('/:bugsRemaining', (req, res)=>{
@@ -53,63 +54,62 @@ app.get('/:bugsRemaining', (req, res)=>{
         res.send(`<h1>Enter a valid number</h1>`)
     }   
     if(num_bugs === 0){
-        const gif = Math.floor(Math.random() * rageGifs.length)
-        res.send(`
-            <h1 style="margin-left: 65vh">Congrats! <span style="color: red">${num_bugs}</span> little bugs in the code!?! Turn in your assignment!
+        const index = Math.floor(Math.random() * rageGifs.length)
+        res.send( 
+            `
+            <h1 style="margin-left: 65vh">Congrats! <span style="color: red">${num_bugs}</span> little bugs in the code <span style="background-image: linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red);   -webkit-background-clip: text;
+            color: transparent">!?!</span> Turn in your assignment!
                 <a href="/" style="${font_size}; ${display}; ${margin_top}; ${margin_left}">Play Again?</a>
-                ${rageGifs[gif]}
+                ${rageGifs[index]}               
             </h1>`
         );
     }else{
+        //set chance for increasing the number of bugs
+        let more_bugs = false;
+        let prev_num = null;
         const percent = 0.02, num_random = +Math.random().toFixed(2);
         console.log("Random Number: " + num_random)
-        const prev_num = num_bugs;
-        let more_bugs = false;
         if(largestNum < CAP){
             largestNum++;
         }
         if(num_random < percent){
-            more_bugs = true;
+            more_bugs = true;   
+            prev_num = num_bugs;
             num_bugs = largestNum;
-        }else{
-            largestNum--;
+                     
         }
         if(num_bugs < 10){
         color = "color: red";
+        }else{
+            color = "color: blue";
         }
 
         if(more_bugs){
-            res.send(`        
-            <h1> <span style="${color}">${prev_num}</span> little bugs in the code.</h1>
-            <h1> <span style="${color}">${prev_num}</span> little bugs</h1>
-            <h1>Take one down.</h1>
-            <p style="color: blue; position: absolute; top: 0px; right: 0px">${num_bugs--}</p>
-            <h1>
-                <a href="/${num_bugs}" style="${display}; ${width}">Pass it around.</a>
-            </h1>
-            <h1><span style="color: red">${num_bugs}</span> little bugs in the code.</h1>`
+            res.send(
+                `
+                <h1> <span style="${color}">${prev_num}</span> little bugs in the code.</h1>
+                <h1> <span style="${color}">${prev_num}</span> little bugs</h1>
+                <h1>Take one down.</h1>
+                <p style="font-size: 0px; color: blue; position: absolute; top: 0px; right: 0px">${num_bugs--}</p>
+                <h1><a href="/${num_bugs}" style="${display};${width}">Pass it around.</a></h1>
+                <h1><span style="color: red">${num_bugs}</span> little bugs in the code.</h1>`
             );
+            
+        }else{     
+            res.send(
+                //main refrain                        
+                `
+                <h1> <span style="${color}">${num_bugs}</span> little bugs in the code.</h1>
+                <h1> <span style="${color}">${num_bugs}</span> little bugs</h1>
+                <h1>Take one down.</h1>
+                <p style="font-size: 0px; color: blue; position: absolute; top: 0px; right: 0px">${num_bugs--}</p>
+                <h1><a href="/${num_bugs}" style="${display};${width}">Pass it around.</a></h1>`           
+            );            
         }
-        res.send(`        
-            <h1> <span style="${color}">${num_bugs}</span> little bugs in the code.</h1>
-            <h1> <span style="${color}">${num_bugs}</span> little bugs</h1>
-            <h1>Take one down.</h1>
-            <p style="color: blue; position: absolute; top: 0px; right: 0px">${num_bugs--}</p>
-            <h1>
-                <a href="/${num_bugs}" style="${display}; ${width}">Pass it around.</a>
-            </h1>`
-        );
     }
    
 });
 
-//localhost:PORT#/98
-app.get(`/98`, (req, res) =>{
-    res.send(`
-        <h1> <span style="${color}: blue">98</span> bottles of ${TYPE} on the wall.</h1> 
-        <h4 style="color: purple">Enter number of bottles in place of <span style="color: blue">98</span> in the url above!</h4>`
-    );
-});
 
 ///////////////////////////
 // Server Listener
